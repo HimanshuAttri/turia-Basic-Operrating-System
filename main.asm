60ac12b1a860d574385d1f1c3c43b75f254d13dc
HimanshuAttri <attri.him@gmail.com>
2020-07-06 22:24:13 +0530

    readDisk complete

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-[org 0x7c00]  ; boot sector starts at this location
+[org 0x7c00]  ;default space for boot sector starts at this location
 mov si, STR  ; It is used as source index for string operations.
 call print
 
@@ -7,6 +7,12 @@ call print
 
 mov si, STR1
 call print
+
+mov al , 1 ; no of sectors to read 
+mov cl , 2 ; bootloader is at sector 1 so we choose latter
+    
+call readDisk
+jmp test_sector2
 
 
 
@@ -33,8 +39,45 @@ print_char:      ; prints the char present at si
 
 
 
+  readDisk:
+    pusha 
+    mov ah, 0x02
+    mov dl,  0x80  ; flopy 0x80 from hard disk , 0x0 for floppy/usb/cd, 
+    mov ch , 0 ; select firstcylinder
+    mov dh, 0 ; select first head
+    push bx 
+    mov bx, 0 
+    mov es ,bx  ; segment register to 0
+    pop bx ; Everything you push, you MUST pop again at some point afterwards, or your code will crash almost immediately!
+    mov bx, 0x7c00 + 512 ; after the boot sector
+    int 0x13
+
+    jc disk_err
+    popa
+    ret
+
+    disk_err:
+      mov si, DISK_ERR_MSG
+      call print 
+      jmp $
+
+
 STR: db "Hello Code" , 0xd, 0 ; db literally places that byte right there in the executable.
 STR0: db "This is Atr-OS 1.0",0x0a, 0x0d, 0
-STR1: db "Learning Right Now..."
-times 510-($-$$) db 0 ; padding and magic number
+
+DISK_ERR_MSG: db "Error Loading Disk.", 0x0a, 0xd, 0
+TEST_STR: db "Welcome to second sector, ",0x0a,0xd,0 
+STR1: db "Learning Right Now...", 0x0a,0xd, 0
+
+
+times 510-($-$$) db 0 ; padding first sector with magic number
+
 dw 0xaa55
+
+test_sector2:
+
+mov si, TEST_STR
+call print 
+
+
+times 512 dw 0 ; padding for the second sector 
